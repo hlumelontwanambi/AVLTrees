3bea70b4182363947070beb273bb91d4d78a04b7
hlumelontwanambi <st10383786@vcconnect.edu.za>
2025-10-27 09:43:08 +0200

    Traversal

--- AVLTrees/AVLTree.cs
+++ AVLTrees/AVLTree.cs
@@ -82,6 +82,53 @@ namespace AVLTrees
 
             //2. Update height of this ancestor node
             node.Height = 1 + Math.Max(GetHeight(node.Left), GetHeight(node.Right));
+
+            // 3. Get the balance factor of this ancestor node
+            int balance = GetBalance(node);
+
+            // 4. If the node becomes unbalanced, then there are 4 cases
+
+            //Left Left cases
+            if (balance > 1 && key < node.Left.Key)
+                return RightRotate(node);
+             // Right Right case
+             if (balance < -1 && key > node.Right.Key) 
+                return LeftRotate(node);
+
+             //Left Right Case
+             if (balance > 1 && key > node.Left.Key)
+            {
+                node.Left = LeftRotate(node.Left);
+                return RightRotate(node);
+            }
+
+             //Right LEft case
+             if (balance < -1 && key < node.Right.Key)
+            {
+                node.Right = RightRotate(node.Right);
+                return LeftRotate(node);
+            }
+
+             // return function to print the tree in-order (sorted)
+            return node;
+        }
+
+        //helper function to print the tree in-order (sorted)
+        public void InOrderTraversal()
+        {
+            Console.WriteLine("\nIn-Order Traversal (sorted view):");
+            InOrderTraversal(root);
+            Console.WriteLine();
+        }
+
+        private void InOrderTraversal(Node node)
+        {
+            if (node != null)
+            {
+                InOrderTraversal(node.Left);
+                Console.Write(node.Key + " ");
+                InOrderTraversal(node.Right);
+            }
         }
     }
 }
